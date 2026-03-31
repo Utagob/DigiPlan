71f4483dcc2823e8b9492444c6f6a0425810fb4c
VictorRudinciuc <rudinciuk1@gmail.com>
2026-03-31 11:11:52 +0200

    Saved changes to events in local storage

--- addevent.js
+++ addevent.js
@@ -1,17 +1,49 @@
 const eventSection = document.getElementsByClassName("eventContainer")[0];
 const addEventButton = document.getElementById("addEventButton");
 
+document.addEventListener("DOMContentLoaded", loadEvents);
 
-function addEvent() {
+function loadEvents() {
+    const savedEvents = localStorage.getItem('events');
+    let eventsArray = [];
+
+    if (savedEvents) {
+        try {
+            eventsArray = JSON.parse(savedEvents);
+        } catch (error) {
+            console.error("Error parsing events from localStorage:", error);
+        }
+    }
+
+    eventSection.innerHTML = '';
+    eventsArray.forEach(eventContent => {
+        addEvent(eventContent);
+    });
+}
+
+function saveEvents() {
+    const events = Array.from(eventSection.children).map(event => {
+        const eventDescription = event.querySelector('.eventDescription');
+        return eventDescription ? eventDescription.innerText : '';
+    });
+    localStorage.setItem('events', JSON.stringify(events));
+}
+
+function addEvent(initialText = "Enter event description...") {
     let newEvent = document.createElement("div");
-    newEvent.id = "event";
+    newEvent.className = "event";
 
     let eventTime = document.createElement("div");
-    eventTime.id = "eventTime";
+    eventTime.className = "eventTime";
     eventTime.innerText = "00:00"; 
 
     let eventDescription = document.createElement("div");
-    eventDescription.id = "eventDescription";
+    eventDescription.className = "eventDescription";
+    eventDescription.innerText = initialText;
+
+    eventDescription.addEventListener("input", () => {
+        saveEvents();
+    });
 
     let deleteEvent= document.createElement("img");
     deleteEvent.id = "deleteEvent";
@@ -19,6 +51,7 @@ function addEvent() {
 
     deleteEvent.addEventListener("click", () => {
         eventSection.removeChild(newEvent);
+        saveEvents();
     });
 
     eventDescription.setAttribute('contenteditable', 'true');
@@ -31,6 +64,6 @@ function addEvent() {
 
 addEventButton.addEventListener("click", () =>{
     addEvent();
-    //save event to local storage
+    saveEvents();
 });
 
